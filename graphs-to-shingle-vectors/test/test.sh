684a82d5cfb1e3cfd0947cd762ed3d3407eabdd9
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2017-05-15 02:36:20 -0400

    test script

---
+++ graphs-to-shingle-vectors/test/test.sh
@@ -0,0 +1,2 @@
+#/usr/bin/env bash
+./streamspot --edges ~/dev/sbustreamspot-cdm/test/test.ss --chunk-length 4 > test/test.sv
---
+++ graphs-to-shingle-vectors/test/test.sv
@@ -0,0 +1,8 @@
+44	4
+shingles	yghg	zgzg	CeBe	CeCe	mgCe	Beme	aame	Reme	yeye	mexe	xeTg	ReRe	yexe	o5xe	Tgme	xeme	mgng	meCe	mezg	Q5xe	aaCg	Tgxe	yeCe	yeme	yeCg	ye	meye	ogye	aaTg	Q5Re	xeye	xexe	aaxe	me	mera	yeRe	Bezg	Tgye	xeDa	zg	Cexe	aaDa	meme	Daye
+1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	1	0	0	0	1	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+2	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	2	0	0	0	0	0	1	0	1	0	2	0	0	0	0	0	0	2	0	1	0	0	1	0	0	0	0	1	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	2	1	0	0	1	0	1	0	0	1	0	1	0	1	0	0	1	0	0	0	0	0
+5	0	0	0	0	0	0	0	1	1	0	1	1	2	1	1	0	0	0	0	1	0	0	0	0	0	0	1	0	0	1	4	0	0	0	0	0	0	1	0	0	0	1	1	1
+3	0	1	2	1	0	1	1	0	0	1	0	0	0	0	0	1	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	2	0	0	1	1	0	0	0
+4	1	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0
