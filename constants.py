9b693b9d39761f4986123dacddbfec9934d72842
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2017-05-18 14:20:02 -0400

    switch to kmeans

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 SEED = 42
-NUM_TRIALS = 20
+NUM_TRIALS = 5
 NUM_DEVS = 3.
--- create_seed_clusters.py
+++ create_seed_clusters.py
@@ -4,9 +4,12 @@ import argparse
 from constants import *
 import numpy as np
 import random
-from medoids import _k_medoids_spawn_once, k_medoids
-from scipy.spatial.distance import pdist, squareform
+import scipy
+import sklearn.preprocessing
 from sklearn.metrics import silhouette_score
+from sklearn.cluster import MiniBatchKMeans
+import sys
+from time import time
 
 # finding best number of clusters
 # http://stackoverflow.com/questions/15376075/cluster-analysis-in-r-determine-the-optimal-number-of-clusters/15376462#15376462
@@ -24,6 +27,7 @@ with open(input_file, 'r') as f:
     num_shingles, chunk_length = map(int, f.readline().strip().split('\t'))
     shingles = f.readline().strip().split('\t')[1:]
 
+    # read shingle count vectors from file
     X = [] # note: row i = graph ID i
     graph_ids = []
     for idx, line in enumerate(f):
@@ -32,78 +36,66 @@ with open(input_file, 'r') as f:
         graph_ids.append(graph_id)
         shingle_vector = map(int, fields[1:])
         X.append(shingle_vector)
+    X = np.array(X, dtype=np.float)
 
-    X = np.array(X)
-    dists = squareform(pdist(X, metric='cosine'))
-    def distance(a, b):
-        return dists[a][b]
-
+    # L2-normalize shingle-count vectors
+    X = sklearn.preprocessing.normalize(X, norm='l2', axis=1, copy=True)
+   
+    # kmeans
     best_n_clusters = -1
     best_silhouette_avg = -1
     best_cluster_labels = None
     best_cluster_centers = None
     for n_clusters in range(2, X.shape[0]):
-        for trial in range(NUM_TRIALS):
-            # run many trials for a given number of clusters
-            _, medoids = _k_medoids_spawn_once(points=range(X.shape[0]),
-                                               k=n_clusters,
-                                               distance=distance,
-                                               max_iterations=1000,
-                                               verbose=False)
-            cluster_labels = [-1] * X.shape[0]
-            for medoid_idx, medoid in enumerate(medoids):
-                graphs = medoid.elements
-                for graph in graphs:
-                    cluster_labels[graph] = medoid_idx 
-            cluster_labels = np.array(cluster_labels)
+        km = MiniBatchKMeans(n_clusters=n_clusters, init='k-means++', n_init=5,
+                             init_size=1000, batch_size=1000, verbose=False,
+                             random_state=SEED)
+        #print 'Clustering, k =', n_clusters,
+        #t0 = time()
+        km.fit(X)
+        #print 'done in %0.3fs.' % (time() - t0),
 
-            # HACK: If only 1 cluster found (should not happen w/ good training)
-            actual_n_clusters = len(medoids)
-            if actual_n_clusters == 1:
-                continue
+        silhouette_avg = silhouette_score(X, km.labels_)
+        #print 'Silhouette Coefficient: %0.3f' % silhouette_avg
 
-            silhouette_avg = silhouette_score(X, cluster_labels, metric='cosine')
-            #print n_clusters, trial, 'silhouette score =', silhouette_avg
+        if silhouette_avg > best_silhouette_avg or\
+           (silhouette_avg == best_silhouette_avg and\
+            n_clusters > best_n_clusters): # favour more clusters
+            best_silhouette_avg = silhouette_avg
+            best_n_clusters = n_clusters
+            best_cluster_labels = km.labels_
+            best_cluster_centers = km.cluster_centers_
 
-            if silhouette_avg > best_silhouette_avg or\
-               (silhouette_avg == best_silhouette_avg and\
-                n_clusters > best_n_clusters): # favour more clusters
-                best_silhouette_avg = silhouette_avg
-                best_n_clusters = n_clusters
-                best_cluster_labels = cluster_labels
-                best_cluster_centers = medoids
+            # compute radius of each cluster
+            all_cluster_dists = []
+            cluster_threshold = [-1] * len(best_cluster_centers) 
+            for cluster_idx in range(best_n_clusters):
+                cluster_center = best_cluster_centers[cluster_idx]
+                cluster_graphs = [i for i in range(X.shape[0])
+                                  if best_cluster_labels[i] == cluster_idx]
+                cluster_dists = [scipy.spatial.distance.cosine(cluster_center, X[i])
+                                 for i in cluster_graphs]
+                all_cluster_dists.extend(cluster_dists)
 
-    all_cluster_dists = []
-    cluster_threshold = [-1] * len(best_cluster_centers) 
-    for cluster_idx in range(len(best_cluster_centers)):
-        cluster_center = best_cluster_centers[cluster_idx].kernel
-        cluster_graphs = best_cluster_centers[cluster_idx].elements
+                mean_dist = np.mean(cluster_dists)
+                std_dist = np.std(cluster_dists)
+                cluster_threshold[cluster_idx] = min(1.0, mean_dist +\
+                                                     NUM_DEVS * std_dist)
+            mean_all_cluster_dists = np.mean(all_cluster_dists)
+            std_all_cluster_dists = np.mean(all_cluster_dists)
+            all_cluster_threshold = min(1.0, mean_all_cluster_dists +\
+                                        NUM_DEVS * std_all_cluster_dists)
 
-        cluster_dists = [dists[cluster_center][graph] for graph in cluster_graphs
-                         if graph != cluster_center]
-        all_cluster_dists.extend(cluster_dists)
+            print str(best_n_clusters) + '\t' + str(X.shape[0]) + '\t',
+            print str(chunk_length) + '\t',
+            print "{:3.4f}".format(all_cluster_threshold) + '\t',
+            print "{:3.4f}".format(best_silhouette_avg)
 
-        # taking mean of cosine distances? seems to be ok
-        # see https://en.wikipedia.org/wiki/Rocchio_algorithm
-        mean_dist = np.mean(cluster_dists)
-        std_dist = np.std(cluster_dists)
-
-        if len(cluster_dists) == 0: # singleton clusters, shouldnt happen
-            mean_dist = 0.0
-            std_dist = 0.0
-            all_cluster_dists.append(0.0)
-
-        cluster_threshold[cluster_idx] = mean_dist + NUM_DEVS * std_dist # P(>) <= 10%
-    mean_all_cluster_dists = np.mean(all_cluster_dists)
-    std_all_cluster_dists = np.mean(all_cluster_dists)
-    all_cluster_threshold = mean_all_cluster_dists + NUM_DEVS * std_all_cluster_dists
-
-    print str(len(best_cluster_centers)) + '\t' + str(X.shape[0]) + '\t',
-    print str(chunk_length) + '\t',
-    print "{:3.4f}".format(all_cluster_threshold)
-
-    for cluster_idx in range(len(best_cluster_centers)):
-        cluster_graphs = best_cluster_centers[cluster_idx].elements
-        threshold = cluster_threshold[cluster_idx]
-        print "{:3.4f}".format(threshold) + '\t',
-        print '\t'.join([str(graph_ids[graph]) for graph in cluster_graphs])
+            for cluster_idx in range(best_n_clusters):
+                cluster_graphs = [i for i in range(X.shape[0])
+                                  if best_cluster_labels[i] == cluster_idx]
+                threshold = cluster_threshold[cluster_idx]
+                print "{:3.4f}".format(threshold) + '\t',
+                print '\t'.join([str(graph_ids[graph]) for graph in cluster_graphs])
+            print
+            sys.stdout.flush()
