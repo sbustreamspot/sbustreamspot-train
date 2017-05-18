c13a00588cc72582b076208853df783ea72dc4ec
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2017-05-18 13:06:50 -0400

    restructure folder

--- graphs-to-shingle-vectors/test/test.sh
+++
@@ -1,2 +0,0 @@
-#/usr/bin/env bash
-./streamspot --edges ~/dev/sbustreamspot-cdm/test/test.ss --chunk-length 4 > test/test.sv
---
+++ test/test.sh
@@ -0,0 +1,2 @@
+#/usr/bin/env bash
+graphs-to-shingle-vectors/streamspot --edges ~/dev/sbustreamspot-cdm/test/test.ss --chunk-length 4 > test/test.sv
